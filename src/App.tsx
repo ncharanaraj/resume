e535d8e4ae053495966330f214d237092bb2a48d
charanarajn <charanaraj.n@wegren.com>
2025-07-07 21:30:00 +0530

    feat: restore Header component from HeaderContainer for improved functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
-import Header from "./components/header/HeaderContainer";
+import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import AppLayout from "./components/ui/AppLayout";
 
